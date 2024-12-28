c8c26f0699944a9984344cbf517d1827cae395a3
mhdshefin <rishnabinthsalam@gmail.com>
2024-12-28 21:36:31 +0530

    updated design

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -13,9 +13,9 @@ const About = () => {
         <img className="opacity-[0.3] max-w-full h-auto" src={assets.bg_shape_bubble} alt="" />
       </div>
       <div className="sm:flex sm:mt-[-25vh] mt-[-35vh]">
-        <div className="sm:w-1/3 w-full sm:pl-10 p-10 flex items-center justify-center sm:mt-0 mt-[-10vh]">
+        <div className="sm:w-1/3 w-full sm:pl-10 p-10 flex items-center justify-center sm:mt-0 mt-[-5vh]">
           <img
-            className="rounded-xl sm:h-[40vh] h-[30vh] relative z-10 shadow-md max-w-full"
+            className="rounded-xl sm:h-[40vh] h-[30vh] relative z-10 shadow-md max-w-full sm:mt-0 mt-[-6vh]"
             src={assets.about_img}
             alt=""
           />
