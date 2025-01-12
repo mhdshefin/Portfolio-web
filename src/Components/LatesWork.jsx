e9ae9d7164dd8bb4bda45652405358fc5945ce34
mhdshefin <rishnabinthsalam@gmail.com>
2025-01-12 15:42:47 +0530

    animation added

--- src/Components/LatesWork.jsx
+++ src/Components/LatesWork.jsx
@@ -32,8 +32,8 @@ const LatesWork = ({ mode }) => {
                     initial={{ y: 40, opacity: 0 }}
                     whileInView={{ y: 0, opacity: 1 }}
                     transition={{ duration: 1, delay: 0.2 }}
-                    className='grid max-w-[100%] px-10 sm:px-20 gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-20 z-20'>
-                    {[assets.my_work2, assets.my_work1, assets.my_work2, assets.my_work1].map((src, index) => (
+                    className='grid max-w-[100%] px-10 sm:px-20 gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-20 z-20'>
+                    {[assets.my_work2, assets.my_work1, assets.my_work2].map((src, index) => (
                         <div key={index} className="relative w-[100%] lg:h-[18vh] xl:h-[25vh] rounded-lg overflow-hidden group border-2 shadow-lg">
                             <img className="w-full h-full object-cover transition-all duration-500 group-hover:scale-105" src={src} alt="" />
                             <div className="absolute inset-0 bg-red-800 opacity-0 transition-opacity duration-500 group-hover:opacity-75 flex flex-col items-center justify-center">
@@ -48,8 +48,8 @@ const LatesWork = ({ mode }) => {
                     initial={{ y: 40, opacity: 0 }}
                     whileInView={{ y: 0, opacity: 1 }}
                     transition={{ duration: 1, delay: 0.4 }}
-                    className='grid max-w-[100%] px-10 sm:px-20 gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 z-20'>
-                    {[assets.my_work2, assets.my_work1, assets.my_work2, assets.my_work1].map((src, index) => (
+                    className='grid max-w-[100%] px-10 sm:px-20 gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 z-20'>
+                    {[assets.my_work2, assets.my_work1, assets.my_work2].map((src, index) => (
                         <div key={index} className="relative w-[100%] lg:h-[18vh] xl:h-[25vh] rounded-lg overflow-hidden group border-2 shadow-lg">
                             <img className="w-full h-full object-cover transition-all duration-500 group-hover:scale-105" src={src} alt="" />
                             <div className="absolute inset-0 bg-red-800 opacity-0 transition-opacity duration-500 group-hover:opacity-75 flex flex-col items-center justify-center">
@@ -62,13 +62,13 @@ const LatesWork = ({ mode }) => {
             </div>
             {
                 showmore ?
-                    <div className='grid max-w-[100%] px-10 sm:px-20 gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-10 transition-transform z-10 duration-300'>
-                        {[assets.my_work2, assets.my_work1, assets.my_work2, assets.my_work1].map((src, index) => (
+                    <div className='grid max-w-[100%] px-10 sm:px-20 gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-10 transition-transform z-10 duration-300'>
+                        {[assets.my_work2, assets.my_work1, assets.my_work2].map((src, index) => (
                             <div key={index} className="relative w-[100%] lg:h-[18vh] xl:h-[25vh] rounded-lg overflow-hidden group">
                                 <img className="w-full h-full object-cover transition-all duration-500 group-hover:scale-105" src={src} alt="" />
                                 <div className="absolute inset-0 bg-red-800 opacity-0 transition-opacity duration-500 group-hover:opacity-75 flex flex-col items-center justify-center">
                                     <img className='sm:w-[50px] w-[40px]' src={assets.search_icon} alt="" />
-                                    <img src={assets.view} alt="" />
+                                    <img className='w-28' src={assets.view} alt="" />
                                 </div>
                             </div>
                         ))}
